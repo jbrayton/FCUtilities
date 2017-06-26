f2675f291c44e93aece4ab019fa82e45b30ec192
John Brayton <jbrayton@goldenhillsoftware.com>
2017-06-26 19:06:37 -0400

    fix framework export

--- FCUtilities/FCUtilities.h
+++ FCUtilities/FCUtilities.h
@@ -17,3 +17,25 @@ FOUNDATION_EXPORT const unsigned char FCUtilitiesVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <FCUtilities/PublicHeader.h>
 
 
+#import <FCUtilities/FCBasics.h>
+#import <FCUtilities/FCCache.h>
+#import <FCUtilities/FCConcurrentMutableDictionary.h>
+#import <FCUtilities/FCExtensionPipe.h>
+#import <FCUtilities/FCNetworkActivityIndicator.h>
+#import <FCUtilities/FCNetworkImageLoader.h>
+#import <FCUtilities/FCOpenInChromeActivity.h>
+#import <FCUtilities/FCOpenInSafariActivity.h>
+#import <FCUtilities/FCPickerViewController.h>
+#import <FCUtilities/FCReachability.h>
+#import <FCUtilities/FCSheetView.h>
+#import <FCUtilities/FCSimpleKeychain.h>
+#import <FCUtilities/FCWebViewLongPressActivityMenu.h>
+#import <FCUtilities/NSArray+FCUtilities.h>
+#import <FCUtilities/NSData+FCUtilities.h>
+#import <FCUtilities/NSString+FCUtilities.h>
+#import <FCUtilities/NSURL+FCUtilities.h>
+#import <FCUtilities/NSURLSession+FCUtilities.h>
+#import <FCUtilities/UIBarButtonItem+FCUtilities.h>
+#import <FCUtilities/UIColor+FCUtilities.h>
+#import <FCUtilities/UIDevice+FCUtilities.h>
+#import <FCUtilities/UIImage+FCUtilities.h>
